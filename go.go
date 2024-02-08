0b28d8fbc16f7bf51fa8f309a398aa1f2a50ba25
iTrooz <hey@itrooz.fr>
2024-02-08 14:00:38 +0100

    compiled code -> code

--- go.go
+++ go.go
@@ -15,10 +15,10 @@ func GoHandler(w http.ResponseWriter, filepath string) error {
 	stdoutSize := stdout.Len()
 	writtenBytes, err := w.Write(stdout.Bytes())
 	if err != nil {
-		return fmt.Errorf("Failed to write compiled code stdout as response: %w", err)
+		return fmt.Errorf("Failed to write code stdout as response: %w", err)
 	}
 	if writtenBytes != stdoutSize {
-		return fmt.Errorf("Failed to fully write compiled code stdout as response: wrote %v/%v", writtenBytes, stdoutSize)
+		return fmt.Errorf("Failed to fully write code stdout as response: wrote %v/%v", writtenBytes, stdoutSize)
 	}
 
 	return nil
